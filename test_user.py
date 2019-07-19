3e86dc856440452966afc7ea3deada09af17df7a
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-19 16:30:51 +0300

    initialise the object to test the file from

--- test_user.py
+++ test_user.py
@@ -9,12 +9,23 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         '''setup method to run before each testcase
         '''
-        self.new_user=user("daniel","123abc")#create my user object
+        self.new_user= User("daniel","123abc")#create my user object
     
     def test_init(self):
-        self.assertEqual(self.new_user.name,"dan")
+        self.assertEqual(self.new_user.name,"daniel")
         self.assertEqual(self.new_user.password,"123abc")
         '''
         test to check if the object is initialised correctly
         '''
 
+    def test_save_user(self):
+        '''
+        to check if the contact object is saved into
+        '''
+        self.new_user.save_user() #saving a user
+        self.assertEqual(len(User.user_list),1)
+
+        
+if __name__=="__main__":
+    unittest.main()
+    
