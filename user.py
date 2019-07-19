e9783b5e7ff5215ec3e557651ef5926ae975bc6e
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-19 15:47:01 +0300

    initialised the user class and a method to define user properties

---
+++ user.py
@@ -0,0 +1,15 @@
+
+
+class User:
+    '''
+    create a class that generates new instances of user
+    '''
+    def __init__(self,name,password):
+        self.name=name
+        self.password=password
+        '''
+        init method that helps us define the user properties
+        Args
+            self.name:user name
+            self.password:user password
+        '''
