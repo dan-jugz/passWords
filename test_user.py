910fd543bb06202d70ccae90743551b78a5e942a
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-19 15:59:13 +0300

    add a parent class from which we can run the various testcases from

---
+++ test_user.py
@@ -0,0 +1,11 @@
+import unittest
+from user import User
+ 
+class TestUser(unittest.TestCase):
+    '''
+    test class that defines testcases for the user class behaviour
+    Parent class that helps create test cases
+    '''
+
+  
+
