16d3449ad74f44e4d21750ebabb32b18a64490e9
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-19 16:20:27 +0300

    initialise the object to test the file from

--- test_user.py
+++ test_user.py
@@ -6,6 +6,15 @@ class TestUser(unittest.TestCase):
     test class that defines testcases for the user class behaviour
     Parent class that helps create test cases
     '''
-
-  
+    def setUp(self):
+        '''setup method to run before each testcase
+        '''
+        self.new_user=user("daniel","123abc")#create my user object
+    
+    def test_init(self):
+        self.assertEqual(self.new_user.name,"dan")
+        self.assertEqual(self.new_user.password,"123abc")
+        '''
+        test to check if the object is initialised correctly
+        '''
 
