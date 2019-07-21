ab8361c082d4226110e22a44e1c3952e88991b3a
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-21 06:10:33 +0300

    add tests to save and delete information from the credentials class

--- cred.py
+++ cred.py
@@ -5,3 +5,15 @@ class Info:
         self.facebk_pass=facebk_pass
         self.email_pass=email_pass
 
+
+    def save_info(self):
+        '''
+        method to save credentials
+        '''
+        Info.info_list.append(self)
+
+    def delete_info(self):
+        '''
+        method to delete credentials
+        '''
+        Info.info_list.remove(self)
--- test_cred.py
+++ test_cred.py
@@ -2,7 +2,7 @@ import unittest
 from cred import Info
 
 
-class CredTest(unittest.TestCase):
+class TestCred(unittest.TestCase):
     def setUp(self):
         self.new_info=Info("fbpass","emailpass")
 
@@ -10,3 +10,24 @@ class CredTest(unittest.TestCase):
         self.assertEqual(self.new_info.facebk_pass,"fbpass")
         self.assertEqual(self.new_info.email_pass,"emailpass")
 
+    def test_save_info(self):
+        '''
+        test if user info is saved
+        '''
+        self.new_info.save_info()
+        self.assertEqual(len(Info.info_list),1)
+
+    def tearDown(self):
+        Info.info_list = []
+
+    def test_delete_info(self):
+        self.new_info.save_info()
+        test_info = Info("briansfb","briansem")
+        test_info.save_info()
+        test_info.delete_info()
+        self.assertEqual(len(Info.info_list),1)
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
